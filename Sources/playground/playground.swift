4241cb856032d06723d0f86f9f624412e98c8b97
Denis Kozlov <deniskozlov@outlook.com>
2025-08-29 12:38:40 +0200

    Added greeting tool

--- Sources/playground/playground.swift
+++ Sources/playground/playground.swift
@@ -1,2 +1,12 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+public struct PlayGreeter {
+    public func greet(name: String) {
+        let locale = "de"
+        do {
+            let greeting = try Phrasebook.word(for: locale)
+            print("\(greeting) \(name)! Welcome to the Swift playground.")
+        } catch {
+            print("Error getting greeting: \(error)")
+            print("\(name)! Welcome to the Swift playground.")
+        }
+    }
+}
