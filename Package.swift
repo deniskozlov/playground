1a97b74d8a5ccfb47fa42dc954ab0c06a4ee25f3
Denis Kozlov <deniskozlov@outlook.com>
2025-08-29 12:23:36 +0200

    Added apple collection dependencies

--- Package.swift
+++ Package.swift
@@ -14,11 +14,17 @@ let package = Package(
             name: "playground",
             targets: ["playground"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "playground",
+            dependencies: [
+                .product(name: "Collections", package: "swift-collections")
+            ],
             resources: [.process("Resources")]
         ),
         .testTarget(
